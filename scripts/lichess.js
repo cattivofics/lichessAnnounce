ce5ad33fb008572118619acc0d8d70112561f618
cattivofics <cattivofics@gmail.com>
2016-03-06 12:23:12 +0100

    added languages

--- scripts/lichess.js
+++ scripts/lichess.js
@@ -1,69 +1,100 @@
-var sounds = new Array();
-sounds['R'] = 'Rook.wav'
-sounds['Q'] = 'Queen.wav'
-sounds['N'] = 'Knight.wav'
-sounds['B'] = 'Bishop.wav'
-sounds['K'] = 'King.wav'
-sounds['x'] = 'Takes.wav'
-sounds['a'] = 'A.wav'
-sounds['b'] = 'B.wav'
-sounds['c'] = 'C.wav'
-sounds['d'] = 'D.wav'
-sounds['e'] = 'E.wav'
-sounds['f'] = 'F.wav'
-sounds['g'] = 'G.wav'
-sounds['h'] = 'H.wav'
-sounds['1'] = '1.wav'
-sounds['2'] = '2.wav'
-sounds['3'] = '3.wav'
-sounds['4'] = '4.wav'
-sounds['5'] = '5.wav'
-sounds['6'] = '6.wav'
-sounds['7'] = '7.wav'
-sounds['8'] = '8.wav'
-sounds['s'] = 'sc.wav'
-sounds['l'] = 'lc.wav'
-
 var moveLetters = new Array();
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+var language = null;
+var announceId = null;
+var sounds = new Array();
 
-setInterval(function(){ announceMove(); }, 500);
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    for (key in changes) {
+        if (key == 'language'){
+            var storageChange = changes[key];
+            language = storageChange.newValue
+        }
+    }
+});
+
+var init = function (){
+    sounds['R'] = 'Rook.wav'
+    sounds['Q'] = 'Queen.wav'
+    sounds['N'] = 'Knight.wav'
+    sounds['B'] = 'Bishop.wav'
+    sounds['K'] = 'King.wav'
+    sounds['x'] = 'Takes.wav'
+    sounds['a'] = 'A.wav'
+    sounds['b'] = 'B.wav'
+    sounds['c'] = 'C.wav'
+    sounds['d'] = 'D.wav'
+    sounds['e'] = 'E.wav'
+    sounds['f'] = 'F.wav'
+    sounds['g'] = 'G.wav'
+    sounds['h'] = 'H.wav'
+    sounds['1'] = '1.wav'
+    sounds['2'] = '2.wav'
+    sounds['3'] = '3.wav'
+    sounds['4'] = '4.wav'
+    sounds['5'] = '5.wav'
+    sounds['6'] = '6.wav'
+    sounds['7'] = '7.wav'
+    sounds['8'] = '8.wav'
+    sounds['O-O'] = 'sc.wav'
+    sounds['O-O-O'] = 'lc.wav'
+    sounds['+'] = 'Check.wav'
+    sounds['#'] = 'Checkmate.wav'
+
+    var config = {
+        subtree: true,
+        childList: true
+    };
+
+    var target = document.querySelector('.replay');
+
+    window.console.log('started');
+    observer.observe(target, config);
+
+    chrome.storage.sync.get('language', function(items) {
+        language = items['language'];
+    });
+
+    if ( language == null ) {
+        language = 'en';
+    }
+};
+
 
 function announceMove () {
-    console.log(moveLetters)
-    if(moveLetters.length > 0){
-        file = 'sounds/' + sounds[moveLetters[0]]
+    if( moveLetters.length > 0 ){
+        file = 'sounds/' + language + '/' + sounds[moveLetters[0]]
         console.log(file)
         var audio = new Audio(chrome.extension.getURL(file))
         audio.play()
         moveLetters.shift()
     }
+    else {
+        clearInterval(announceId);
+    }
 }
 
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-
 var observer = new MutationObserver(function( mutations ) {
+
     lastMove = $(document).find('move.active');
-    if(lastMove != null){
+
+    if ( lastMove != null ) {
         move = lastMove[0].textContent
-        if(move == 'O-O'){
-            moveLetters.push('s')
+        if ( move == null ) {
             return;
         }
-        if(move == 'O-O-O'){
-            moveLetters.push('l')
-            return;
+        if ( move == 'O-O' || move == 'O-O-O' ) {
+            moveLetters.push(move);
         }
-        moveLetters = move.split('')
+        else {
+            moveLetters = move.split('');
+        }
+    }
+
+    if ( moveLetters.length > 0 ) {
+        announceId = setInterval(function(){ announceMove(); }, 700);
     }
 });
 
-var config = {
-  subtree: true,
-  childList: true
-};
-
-var target = document.querySelector('.replay');
-
-window.console.log('started');
-observer.observe(target, config);
+init();
 
